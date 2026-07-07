0977d6c29b32f02d1931b2adcf3bf73e74d4462d
srivolous <sreeragmanoj5@gmail.com>
2026-07-07 19:38:39 +0530

    added time

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,40 @@
+use chrono::{DateTime,Local};
+
 struct PID{
     kp: f64,
     ki: f64,
-    kd: f64
+    kd: f64,
     i:f64,
     p_err:f64,
+    p_time:DateTime<Local>,
 }
+
 impl PID{
     fn new(kp:f64,ki:f64,kd:f64)->Self{
         Self{
-            kp,ki,kd,i:0.0,p_err:0.0
+            kp,ki,kd,i:0.0,p_err:0.0,p_time:Local::now(),
         }
     }
-    fn calculate(&mut self, setzeug:f64, reading:f64,dt:f64)->f64{
+    fn calculate(&mut self, setzeug:f64, reading:f64)->f64{
         let error = setzeug - reading;
+        let dtt = Local::now() - self.p_time;
+        let dt = dtt.num_milliseconds() as f64/1000.0;
         let p = self.kp*error;
         self.i +=error*dt;
         let i_o = self.ki*self.i;
         let d = (error-self.p_err)/dt;
-        let d_o = self.kd*derivative;
-        self.p_err = err;
+        let d_o = self.kd*d;
+        self.p_err = error;
+        self.p_time = Local::now();
         p + i_o + d_o
     }
 }
 fn main() {
-    println!("hello waters");
-
+    //let aa = Local::now();
+    let mut n = PID::new(0.1,0.01,0.0);
+    std::thread::sleep(std::time::Duration::from_millis(10));
+    let ree = n.calculate(2.0,5.0);
+    //println!("is reading {}",ree);
+    let a = Local::now();
+    //println!("local time {}",(a-aa).num_seconds());
 }
